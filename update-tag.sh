35d6ef960266d78ef45e69f816d1fe9a3b5f4388
Dórian Langbeck <dorianlangbeck@gmail.com>
2023-06-01 15:03:57 -0300

    Update update-tag.sh script

--- update-tag.sh
+++ update-tag.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -e -o pipefail
 
-if [ $# -ne 1 ]; then
+SUPPORTED_COLORS=(red orange yellow green blue purple)
+
+show_usage_and_exit() {
     echo >&2 "Usage: ${0} <color-tag>"
-    echo >&2 "Supported colors are: red, orange, yellow, green, blue and purple."
+    echo >&2 "Supported colors are: ${SUPPORTED_COLORS[@]}"
     exit 1
-fi
+}
+
+[ $# -ne 1 ] && show_usage_and_exit
 
 NEW_TAG="${1}"
+found=0
+for color in "${SUPPORTED_COLORS[@]}"; do
+    if [[ "${color}" == "${NEW_TAG}" ]]; then
+        found=1
+        break
+    fi
+done
+
+[ "${found}" -eq 0 ] && show_usage_and_exit
+
 
 git checkout apps/rollout-exp/kustomization.yaml
 sed -i "s/newTag:.*/newTag: ${NEW_TAG}/" apps/rollout-exp/kustomization.yaml
